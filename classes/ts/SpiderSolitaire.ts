a32770c050ba910df47801acc8fe69267ca28b3d
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-08-19 17:49:18 +0000

    next initialization

--- classes/ts/SpiderSolitaire.ts
+++ classes/ts/SpiderSolitaire.ts
@@ -22,14 +22,31 @@ export class SpiderSolitaire implements ISpiderSolitaire{
 
     initializeGame(): void {
         // Lógica para distribuir las cartas en las columnas
+        this.columns.forEach(column => {
+            for (let i = 0; i < 4; i++) { // Asume 4 cartas en cada columna inicialmente
+                const card = this.deck.draw();
+                if (card) {
+                    column.addCard(card);
+                }
+            }
+        });
     }
 
-    moveCard(fromColumn: number, toColumn: number, card: Card): void {
+    moveCard(fromColumnIndex: number, toColumnIndex: number, card: Card): void{
         // Implementar lógica para mover cartas entre columnas
+        const fromColumn = this.columns[fromColumnIndex];
+        const toColumn = this.columns[toColumnIndex];
+
+        // Validación básica para el movimiento (puede necesitar ajustes según las reglas específicas)
+        if (fromColumn && toColumn) {
+            fromColumn.removeCard(card);
+            toColumn.addCard(card);
+        }
     }
 
     hint(): void {
         // Implementar lógica para proporcionar una sugerencia
+        console.log('Lo siento, las sugerencias aún no están implementadas en este juego.');
     }
 
     // Más métodos según sea necesario para representar las reglas y el estado del juego
